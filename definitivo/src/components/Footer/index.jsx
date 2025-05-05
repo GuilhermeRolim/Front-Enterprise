c174893b2bfe7421b4b816a646a0e8802374c370
Guilherme Forchito Rolim - 82414858 <rolimguilherme14@gmail.com>
2025-05-05 20:44:20 -0300

    feat: improved footer modules and updated index footer structure

---
+++ definitivo/src/components/Footer/index.jsx
@@ -0,0 +1,10 @@
+import styles from './Footer.module.css'
+function Footer () {
+    return( 
+        <footer className={styles.Footer}>
+            Todos os Diretitos Reservados &copy; 2025
+        </footer>
+    )
+}
+
+export default Footer 
