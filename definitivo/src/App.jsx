74e87506721ecd80ec27d1bd06e2a499680c01f0
Guilherme Forchito Rolim - 82414858 <rolimguilherme14@gmail.com>
2025-05-05 20:46:30 -0300

    feat: update footer section in App.jsx

--- definitivo/src/App.jsx
+++ definitivo/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Footer from './components/Footer'
 import Header from './components/Header'
 
 function App() {
@@ -6,6 +7,24 @@ function App() {
   return (
     <>
       <Header />
+      <section className='container'>
+        <div>
+          <p>
+            Olá, somos <br/>
+            <span> Guilherme Rolim</span> <br/>
+            <span> Gustavo Catani</span> <br/>
+            <span> Plinio Augusto</span> <br/>
+            Estudantes de análise e Desenvolvimento de Sistemas
+          </p>
+          <button>
+            Saiba mais sobre nós
+          </button>
+        </div>
+        <figure>
+          <img src="" alt="" />
+        </figure>
+      </section>       
+      <Footer/>
     </>
   )
 }
