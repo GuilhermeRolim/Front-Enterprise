b61eaba5cd7b55096cb2ad9fbce5a23bb20b4fa5
Guilherme Rolim <rolimguilherme14@gmail.com>
2025-05-13 00:09:06 -0300

    Feat: in the pages folder, there are 4 subfolders, each of which has its own index, within the index the beginning of each page was created

---
+++ definitivo/src/pages/Home/index.jsx
@@ -0,0 +1,28 @@
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+function Home (){
+    return(
+        <>
+            <Header />
+            <section className='container'>
+                <div className='apresentacao'>
+                <p>
+                    Olá, seja bem-vindo <br/>
+                    <span> somos a Front-Enterprise</span> <br/>
+                    uma empresa de tecnologia <br />
+                    focada em soluções financeiras a.
+                </p>
+                <button className='btn btn-red'>
+                    Saiba mais sobre nós
+                </button>
+                </div>
+                <figure>
+                <img className='img-home' src="/undraw_mobile-payments_0u42.svg" alt="Imagem de  home" />
+                </figure>
+            </section>       
+            <Footer/>
+        </>
+    )
+}
+
+export default Home
