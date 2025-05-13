b1b58a5c97b0c82756418873f69b14a0be727383
Guilherme Rolim <rolimguilherme14@gmail.com>
2025-05-13 01:11:17 -0300

    chore: code update performed without changing functionality

--- definitivo/src/pages/Home/index.jsx
+++ definitivo/src/pages/Home/index.jsx
@@ -1,29 +1,35 @@
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 import { Link } from 'react-router-dom'
+import Container from '../../components/Container'
+import styles from './Home.module.css'
+
 function Home (){
     return(
         <>
             <Header />
-            <section className='container'>
-                <div className='apresentacao'>
-                <p>
-                    Olá, seja bem-vindo <br/>
-                    <span> somos a Front-Enterprise</span> <br/>
-                    uma empresa de tecnologia <br />
-                    focada em soluções financeiras a.
-                </p>
-                <Link to="/sobre" className='btn btn-red'>
-                    Saiba mais sobre nós
-                </Link>
-                </div>
-                <figure>
-                <img className='img-home' src="/undraw_mobile-payments_0u42.svg" alt="Imagem de  home" />
-                </figure>
-            </section>       
+            <Container>
+                 <section className={styles.home}>
+                    <div className={styles.apresentacao}>
+                    <p>
+                        Olá, seja bem-vindo <br/>
+                        <span> somos a Front-Enterprise</span> <br/>
+                        uma empresa de tecnologia <br />
+                        focada em soluções financeiras a.
+                    </p>
+                    <Link to="/sobre" className={`${styles.btn} ${styles.btn_red}`}>
+                        Saiba mais sobre nós
+                    </Link>
+                    </div>
+                    <figure>
+                    <img className={styles.img_home} src="/undraw_mobile-payments_0u42.svg" alt="Imagem de  home" />
+                    </figure>
+            </section>     
+            </Container>
             <Footer/>
         </>
     )
 }
 
 export default Home
+ 
