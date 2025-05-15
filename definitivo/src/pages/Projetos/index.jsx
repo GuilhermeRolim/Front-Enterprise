4b6449be11eba4440adf095febdadc9189b0db07
Guilherme Rolim <rolimguilherme14@gmail.com>
2025-05-14 23:33:05 -0300

    Feat: the css from the about page was imported and the image link was added

--- definitivo/src/pages/Projetos/index.jsx
+++ definitivo/src/pages/Projetos/index.jsx
@@ -1,14 +1,16 @@
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 import Container from '../../components/Container'
+import './projetos.modules.css';
 
 function Projetos(){
-   
     return(
         <>
             <Header /> 
                 <Container>
+                    <section className="projetos-section"></section>
                     <h2>Projetos</h2>
+                    <img src="undraw_mobile-payments_0u42.svg" alt="Imagem de projetos" className="projetos-image" />
                 </Container>
             <Footer />
         </>
